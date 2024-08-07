27aaa9d652de03f1e409420a97e58c75db727aba
Param Pathak <parampathak47@gmail.com>
2024-08-08 01:22:30 +0530

    Update config.go

--- config/config.go
+++ config/config.go
@@ -14,28 +14,23 @@ var Client *mongo.Client
 var BlogCollection *mongo.Collection
 
 func ConnectDB() {
-    // Load environment variables from .env file
     err := godotenv.Load()
     if err != nil {
         log.Fatal("Error loading .env file")
     }
 
-    // Get the MongoDB URI from environment variables
     mongoURI := os.Getenv("MONGODB_URI")
     if mongoURI == "" {
         log.Fatal("MONGODB_URI not set in .env file")
     }
 
-    // Set MongoDB client options
     clientOptions := options.Client().ApplyURI(mongoURI)
 
-    // Connect to MongoDB
     Client, err = mongo.Connect(context.TODO(), clientOptions)
     if err != nil {
         log.Fatal(err)
     }
 
-    // Check the connection
     err = Client.Ping(context.TODO(), nil)
     if err != nil {
         log.Fatal(err)
@@ -43,6 +38,5 @@ func ConnectDB() {
 
     log.Println("Connected to MongoDB!")
 
-    // Get the blog collection
     BlogCollection = Client.Database("blogDB").Collection("posts")
 }
